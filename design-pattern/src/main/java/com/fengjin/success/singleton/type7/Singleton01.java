9ce7fb8118b938924bbda891c7b0ed7e4bfcdb13
GAIAWORKS\Arno.Feng <Arno.Feng@gaiaworks.cn>
2021-07-02 15:01:37 +0800

    单例模式

---
+++ design-pattern/src/main/java/com/fengjin/success/singleton/type7/Singleton01.java
@@ -0,0 +1,29 @@
+package com.fengjin.success.singleton.type7;
+
+/**
+ * @author: Arno.Feng
+ * @date: 2021/7/1
+ * @description:
+ */
+public class Singleton01 {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+        Singleton instance1 = Singleton.getInstance();
+        System.out.println(instance == instance1);
+        System.out.println(instance.hashCode());
+        System.out.println(instance1.hashCode());
+    }
+}
+
+class Singleton {
+
+    private Singleton() {}
+
+    private static class SingletonInstance {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonInstance.INSTANCE;
+    }
+}
